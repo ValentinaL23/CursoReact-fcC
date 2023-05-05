abfa4d259726cef5347594f4f5057cf5bc5ff84b
ValentinaL23 <dayana.largo.p@gmail.com>
2023-05-04 22:49:11 -0500

    testm-cont

---
+++ contador-clicks/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import fccLogo from './imagenes/fcc-logo.png';
+
+function App() {
+  return (
+    <div className='App'>
+       <div className='fcc-logo-contenedor'>
+        <img 
+          className='fcc-logo'
+          src={fccLogo}
+          alt="" />
+       </div>
+    </div>
+  )
+}
+
+export default App
